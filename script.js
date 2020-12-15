e03f85a22e72871dc28440e489ea8a238e07d771
Brandon Craig <thegamingraven13@gmail.com>
2020-12-15 15:28:59 -0800

    Couldn't finish the assignment. Got the quiz to cycle through questions with answer buttons. Couldn't get the timer to subtract time when selecting a wrong answer.

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 var counter = 60;
 var startBtn = document.querySelector("#start-quiz");
-var corrBtn = document.querySelector(".answerCorr");
-
-
+var timeMinus = document.querySelector("#timer");
 
 startBtn.addEventListener("click", startQuiz);
-corrBtn.addEventListener("click", answerCorr);
-
-
 
 //change x to global so timer can stop when quiz is finished
 
@@ -24,7 +19,7 @@ function startQuiz() {
             id = document.getElementById("timer");
             id.innerHTML = counter + 61;//change this later if its negative
             counter = 60;
-            alert("It works!!!");
+            console.log("timer works");
 
         }
     }, 1000)
@@ -35,48 +30,49 @@ function startQuiz() {
     getQuiz.style.display = "block";
 }
 
-function clearH3() {
-    document.getElementById("h3").textContent = "";
-    setTimeout(clearH3, 3000);
-}
-
-function answerCorr() {
-    document.getElementById("h3").textContent = "Correct!";
-    setTimeout(clearH3, 3000);
-}
 
 function answerWro() {
-    document.getElementById("h3").textContent = "Wrong!";
+    document.getElementById("timer") - 10;
     counter - 10;
-    id.innerHTML = counter -10;
 }
 
-function nextQues() {
+function nextQues1() {
     if(document.getElementById("quiz1").style.display === "block") {
         document.getElementById("quiz1").style.display = "none";
         document.getElementById("quiz2").style.display = "block";
-    } else if (document.getElementById("quiz2").style.display === "block") {
-        document.getElementById("quiz2").style.display = "none";
-        document.getElementById("quiz3").style.display = "block";
-            
-    } else if (document.getElementById("quiz3").style.display === "block") {
-        document.getElementById("quiz3").style.display = "none";
-        document.getElementById("quiz4").style.display = "block";
-            
-    } else if (document.getElementById("quiz4").style.display === "block") {
-        document.getElementById("quiz4").style.display = "none";
-        document.getElementById("quiz5").style.display = "block";
-            
-    } else (document.getElementById("quiz5").style.display === "block") {
-        document.getElementById("results").style.display = "none";
-        document.getElementById("results").style.display = "block";    
     }
 }
 
+function nextQues2() {
+    if (document.getElementById("quiz2").style.display === "block") {
+        document.getElementById("quiz2").style.display = "none";
+        document.getElementById("quiz3").style.display = "block";
+    }
+}
+    
+function nextQues3() {
+    if (document.getElementById("quiz3").style.display === "block") {
+        document.getElementById("quiz3").style.display = "none";
+        document.getElementById("quiz4").style.display = "block";
+    }
+}
+
+function nextQues4() {
+    if (document.getElementById("quiz4").style.display === "block") {
+        document.getElementById("quiz4").style.display = "none";
+        document.getElementById("quiz5").style.display = "block";
+    }
+}
+
+function nextQues5() {
+    if (document.getElementById("quiz5").style.display === "block"); {
+        document.getElementById("quiz5").style.display = "none";
+        document.getElementById("results").style.display = "block";
+    }
+}
+            
+    
+
+
 //create a loop checking high scores. start with first place
 // array1.sort(); --sets array in order
-
-
-
-
-
