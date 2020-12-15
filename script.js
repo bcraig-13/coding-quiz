09ca52433fe7c6153c72f72448a871192cd8e518
Brandon Craig <thegamingraven13@gmail.com>
2020-12-14 17:18:00 -0800

    Layed the foundation for the questions in the index.html. Timer still works properly.

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 var counter = 60;
-
+var startBtn = document.querySelector("#start-quiz");
+//change x to global so timer can stop when quiz is finished
 
 function startQuiz() {
     var x = setInterval(function () {
         counter--;
-    
+
         if (counter >= 0) {
             id = document.getElementById("timer");
             id.innerHTML = counter;
-    
+
         } else if (counter < 0) {
-        clearInterval(x);
-        id = document.getElementById("timer");
-        id.innerHTML = counter + 61;
-        counter = 60;
-        alert("It works!!!");
-    
+            clearInterval(x);
+            id = document.getElementById("timer");
+            id.innerHTML = counter + 61;//change this later if its negative
+            counter = 60;
+            alert("It works!!!");
+
         }
     }, 1000)
+
+    let getStart = document.querySelector("#start-screen");
+    let getQuiz = document.querySelector("#quiz");
+    getStart.style.display = 'none';
+    getQuiz.style.display = 'block';
 }
 
+//create a loop checking high scores. start with first place
+// array1.sort(); --sets array in order
 
+
+
+
+startBtn.addEventListener("click", startQuiz);
